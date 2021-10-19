da80b5749ac127fabc98bb5243c5f84de3982320
Tanishq-Jainop <78198324+Tanishq-Jainop@users.noreply.github.com>
2021-10-19 20:37:11 +0530

    Add files via upload

---
+++ .js
@@ -0,0 +1,10 @@
+function preload(){
+}
+function setup(){
+    canvas=createCanvas(280,280);
+    canvas.center();
+    background("green");
+}
+function createCanvas(){
+    background("green");
+}
